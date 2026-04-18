f95108702b2a4914eb53c52e9a12a74eaec6f0ca
Meg Dude <me@megdude.com>
2026-04-18 16:33:06 -0500

    Add MapPage route and fix import issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PageNotFound from './lib/PageNotFound';
-import { AuthProvider, useAuth } from '@/lib/AuthContext';
-import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import Layout from './components/Layout';
-import Home from './pages/Home';
+import { Toaster } from "@/components/ui/toaster";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClientInstance } from "@/lib/query-client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PageNotFound from "./lib/PageNotFound";
+import { AuthProvider, useAuth } from "@/lib/AuthContext";
+import UserNotRegisteredError from "@/components/UserNotRegisteredError";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 // Downtown Perks pages
-import Landing from './pages/downtown-perks/Landing';
-import ExploreRebuilt from './pages/downtown-perks/ExploreRebuilt';
-import Events from './pages/downtown-perks/Events';
-import PerksPage from './pages/downtown-perks/PerksPage';
-import PerksCard from './pages/downtown-perks/PerksCard';
-import ForBuildings from './pages/downtown-perks/ForBuildings';
-import About from './pages/downtown-perks/About';
+import Landing from "./pages/downtown-perks/Landing";
+import ExploreRebuilt from "./pages/downtown-perks/ExploreRebuilt";
+import Events from "./pages/downtown-perks/Events";
+import PerksPage from "./pages/downtown-perks/PerksPage";
+import PerksCard from "./pages/downtown-perks/PerksCard";
+import ForBuildings from "./pages/downtown-perks/ForBuildings";
+import About from "./pages/downtown-perks/About";
 // Brands pages
-import BrandsIndex from './pages/downtown-perks/brands/Index';
-import ThePaseo from './pages/downtown-perks/brands/ThePaseo';
-import TheWaterline from './pages/downtown-perks/brands/TheWaterline';
-import Bangers from './pages/downtown-perks/brands/Bangers';
-import TheStayPut from './pages/downtown-perks/brands/TheStayPut';
-import Yeti from './pages/downtown-perks/brands/Yeti';
-import Rivian from './pages/downtown-perks/brands/Rivian';
-import Lululemon from './pages/downtown-perks/brands/Lululemon';
-import Equinox from './pages/downtown-perks/brands/Equinox';
-import AustinFC from './pages/downtown-perks/brands/AustinFC';
-import FabiAndRosi from './pages/downtown-perks/brands/FabiAndRosi';
-import HotelVanZandt from './pages/downtown-perks/brands/HotelVanZandt';
-import FourSeasons from './pages/downtown-perks/brands/FourSeasons';
-import FourSeasonsResidences from './pages/downtown-perks/brands/FourSeasonsResidences';
-import TheShore from './pages/downtown-perks/brands/TheShore';
-import PartnersIndex from './pages/partners/Index';
-import ResidentialPartner from './pages/partners/Residential';
-import CivicPartner from './pages/partners/Civic';
-import PartnerWorkspace from './pages/PartnerWorkspace';
-import Dashboard from './pages/Dashboard';
-import PartnerDashboard from './pages/PartnerDashboard';
-import PropertiesPartner from './pages/partners/Properties';
-import HotelsPartner from './pages/partners/Hotels';
-import VenuesPartner from './pages/partners/Venues';
-import BrandsPartner from './pages/partners/Brands';
-import ResidentApp from './pages/resident-app';
-import BrandAnalytics from './pages/downtown-perks/brands/BrandAnalytics';
+import BrandsIndex from "./pages/downtown-perks/brands/Index";
+import ThePaseo from "./pages/downtown-perks/brands/ThePaseo";
+import TheWaterline from "./pages/downtown-perks/brands/TheWaterline";
+import Bangers from "./pages/downtown-perks/brands/Bangers";
+import TheStayPut from "./pages/downtown-perks/brands/TheStayPut";
+import Yeti from "./pages/downtown-perks/brands/Yeti";
+import Rivian from "./pages/downtown-perks/brands/Rivian";
+import Lululemon from "./pages/downtown-perks/brands/Lululemon";
+import Equinox from "./pages/downtown-perks/brands/Equinox";
+import AustinFC from "./pages/downtown-perks/brands/AustinFC";
+import FabiAndRosi from "./pages/downtown-perks/brands/FabiAndRosi";
+import HotelVanZandt from "./pages/downtown-perks/brands/HotelVanZandt";
+import FourSeasons from "./pages/downtown-perks/brands/FourSeasons";
+import FourSeasonsResidences from "./pages/downtown-perks/brands/FourSeasonsResidences";
+import TheShore from "./pages/downtown-perks/brands/TheShore";
+import PartnersIndex from "./pages/partners/Index";
+import ResidentialPartner from "./pages/partners/Residential";
+import CivicPartner from "./pages/partners/Civic";
+import PartnerWorkspace from "./pages/PartnerWorkspace";
+import Dashboard from "./pages/Dashboard";
+import PartnerDashboard from "./pages/PartnerDashboard";
+import PropertiesPartner from "./pages/partners/Properties";
+import HotelsPartner from "./pages/partners/Hotels";
+import VenuesPartner from "./pages/partners/Venues";
+import BrandsPartner from "./pages/partners/Brands";
+import ResidentApp from "./pages/resident-app";
+import BrandAnalytics from "./pages/downtown-perks/brands/BrandAnalytics";
+// New Map Page import
+import MapPage from "./pages/Map";
 
 const AuthenticatedApp = () => {
   const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();
@@ -58,9 +60,9 @@ const AuthenticatedApp = () => {
 
   // Handle authentication errors
   if (authError) {
-    if (authError.type === 'user_not_registered') {
+    if (authError.type === "user_not_registered") {
       return <UserNotRegisteredError />;
-    } else if (authError.type === 'auth_required') {
+    } else if (authError.type === "auth_required") {
       // Redirect to login automatically
       navigateToLogin();
       return null;
@@ -97,10 +99,11 @@ const AuthenticatedApp = () => {
         <Route path="/brands/four-seasons" element={<FourSeasons />} />
         <Route path="/brands/four-seasons-residences" element={<FourSeasonsResidences />} />
         <Route path="/brands/the-shore" element={<TheShore />} />
+        {/* Partners routes */}
         <Route path="/partners" element={<PartnersIndex />} />
         <Route path="/partners/civic" element={<CivicPartner />} />
         <Route path="/partners/residential" element={<ResidentialPartner />} />
-        <Route path="/partners/properties" element={<ResidentialPartner />} />
+        <Route path="/partners/properties" element={<PropertiesPartner />} />
         <Route path="/partner-workspace" element={<PartnerWorkspace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/partner-dashboard" element={<PartnerDashboard />} />
@@ -108,15 +111,16 @@ const AuthenticatedApp = () => {
         <Route path="/partners/venues" element={<VenuesPartner />} />
         <Route path="/partners/brands" element={<BrandsPartner />} />
         <Route path="/resident-app" element={<ResidentApp />} />
+        {/* Map page route */}
+        <Route path="/map" element={<MapPage />} /> {/* New Map Route */}
+        {/* Catch-all route */}
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
   );
 };
 
-
 function App() {
-
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
@@ -126,7 +130,7 @@ function App() {
         <Toaster />
       </QueryClientProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
