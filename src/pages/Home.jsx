7f3a8645d2dbb9fd0378ed0cd52a75993098e217
Meg Dude <jfw6fhmd4g@privaterelay.appleid.com>
2026-04-30 02:55:07 -0500

    Pearl UI: make homepage map-first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,5 @@
 import MapShell from "@/components/map/MapShell";
-import WhySection from "@/components/home/WhySection";
-import LiveTonight from "@/components/home/LiveTonight";
-import PartnerSlides from "@/components/home/PartnerSlides";
-import PricingSection from "@/components/home/PricingSection";
-import FAQSection from "@/components/home/FAQSection";
-import ContactSection from "@/components/home/ContactSection";
 
 export default function Home() {
-  return (
-    <main>
-      <MapShell mode="home" />
-      <WhySection />
-      <LiveTonight />
-      <PartnerSlides />
-      <PricingSection />
-      <FAQSection />
-      <ContactSection />
-    </main>
-  );
+  return <MapShell mode="home" className="min-h-screen" />;
 }
