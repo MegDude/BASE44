0c85a167f68a6022c75ebf28de396746bee11baf
Meg Dude <jfw6fhmd4g@privaterelay.appleid.com>
2026-07-28 14:30:45 -0500

    fix(platform): clear canonical release blockers

---
+++ src/data/production/canonicalEntityAliasRegistry.ts
@@ -0,0 +1,13 @@
+export const canonicalEntityAliasRegistry = {
+  "map-1593-311-w-5th-st-1005": "map-1563-311-w-5th-st-1005",
+  "map-1603-311-w-5th-st-1008": "map-1564-311-w-5th-st-1008",
+  "map-1604-311-w-5th-st-806": "map-1562-311-w-5th-st-806",
+  "map-1594-311-w-5th-st-unit-806": "map-1579-311-w-5th-st-unit-806",
+  "map-1568-311-w-5th-st-w-902": "map-1566-311-w-5th-st-w-902",
+  "map-1597-311-w-5th-st-w-902": "map-1566-311-w-5th-st-w-902",
+  "map-691-amli-south-shore": "map-690-amli-south-shore",
+  "map-663-banger-s-sausage-house-and-beer-garden": "map-7-banger-s-sausage-house-and-beer-garden",
+  "map-60-green-light-social": "map-59-green-light-social",
+  "map-1064-historic-scoot-inn": "map-65-historic-scoot-inn",
+  "map-1437-target": "map-1435-target"
+} as const;
